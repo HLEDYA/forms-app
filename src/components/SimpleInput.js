ee43148e8ab48bc59a44486c7723ccb747a52200
Yusuf Engin Tetik <yusufengintetik@gmail.com>
2022-08-14 17:06:34 +0300

    basic validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,24 +1,56 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [isNameValid, setIsNameValid] = useState(false);
+  const [isNameTouched, setIsNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (isNameValid) {
+      console.log("name is valid!");
+    }
+  }, [isNameValid]);
 
   const nameInputHandler = (event) => {
     setEnteredName(event.target.value);
+    if (event.target.value.trim() !== "") {
+      setIsNameValid(true);
+    }
+  };
+
+  const nameInputLostFocusHandler = (event) => {
+    setIsNameTouched(true);
+    if (enteredName.trim() === "") {
+      setIsNameValid(false);
+    }
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    setIsNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setIsNameValid(false);
+      return;
+    }
+    setIsNameValid(true);
+
     console.log(enteredName);
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
     setEnteredName("");
   };
 
+  const nameInputInvalid = !isNameValid && isNameTouched;
+
+  const nameInputClasses = nameInputInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -26,7 +58,11 @@ const SimpleInput = (props) => {
           id="name"
           value={enteredName}
           onChange={nameInputHandler}
+          onBlur={nameInputLostFocusHandler}
         />
+        {nameInputInvalid && (
+          <p className="error-text">Name must be non-empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
